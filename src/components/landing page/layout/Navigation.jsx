681040d08029d94e6713f61d76b2df1b3cad84da
Oladele Marwan <oladelemarwan@gmail.com>
2021-11-16 10:09:28 +0000

    landing page

---
+++ src/components/landing page/layout/Navigation.jsx
@@ -0,0 +1,37 @@
+ import React from "react";
+import img from "../../../img/Logo.png";
+import "bootstrap/dist/css/bootstrap.css";
+import { Nav, Navbar } from "react-bootstrap";
+
+const Navigation = () => {
+  return (
+    <div>
+      <Navbar
+        bg="mybg"
+        variant="dark"
+        sticky="top"
+        expand="sm"
+      >
+
+        
+        <Navbar.Brand className="mgn">
+          <img className="logo" src={img} />
+        </Navbar.Brand>
+
+        <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav>
+              <Nav.Link href="#">
+                <button className="styl">Sign in</button>
+              </Nav.Link>
+              <Nav.Link href="#">
+                <button className="styl">Get Started</button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,15 +1,16 @@
 import "../components/styles/landing-page.css";
 import React from "react";
-import Nav from "../components/landing page/layout/Nav";
+import Nav from "../components/landing page/layout/Navigation";
 import Hero from "../components/landing page/layout/Hero";
 import Quality from "../components/landing page/layout/Quality";
 import Footer from "../components/landing page/layout/Footer";
+import Navigation from "../components/landing page/layout/Navigation";
 
 
 const LandingPage = () => {
   return (
     <React.Fragment>
-      <Nav />
+      <Navigation />
       <Hero />
       <Quality />
       <Footer />
