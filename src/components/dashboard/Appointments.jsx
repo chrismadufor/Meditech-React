70510158dcfb683df363dcd9f13848f2cbe9a932
Oladele Marwan <oladelemarwan@gmail.com>
2021-11-17 08:14:54 +0000

    appointment-form

--- src/components/dashboard/Appointments.jsx
+++ src/components/dashboard/Appointments.jsx
@@ -1,66 +1,111 @@
-import React from 'react'
-import TopNav from './layouts/TopNav'
+import React from "react";
+import TopNav from "./layouts/TopNav";
+import Appointment from "../styles/bookAppointment.module.css";
+// import { Field, Formik} from "formik";
 
 function Appointments() {
-    return (
-        <div className='main'>
-            <TopNav name='Appointments'/>
-            <div className='main-content' style = {{display: "none"}}>
-                <div className="tableComponent">
-                    <div className="tableTopRow">
-                    <div>
-                        <select name="label"  className="filterOption">
-                        <option value="all">All</option>
-                        <option value="pending">Pending</option>
-                        <option value="cancelled">Cancelled</option>
-                        <option value="completed">Completed</option>
-                        </select>
-                    </div>
-                    <div>
-                        <img src="./img/Elipses (3).png" className="elipses" alt=''/>
-                    </div> 
-                    </div>
-                    <table>
-                    <thead>
-                        <tr>
-                            <th>
-                            Doctor Assigned</th>
-                            <th>Date</th>
-                            <th>Time</th>
-                            <th>Contact</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody id="table-body-ad">
-                    
-                    </tbody>
-                    </table>
-                    <div className="tableControls">
-                    <div className="control-box">
-                        <p> <i className='fas fa-chevron-left'></i><i className='fas fa-chevron-left'></i> </p>
-                    </div>
-                    <div className="control-box">
-                        <p> <i className='fas fa-chevron-left'></i> </p>
-                    </div>
-                    <div className="control-box">
-                    <p>1</p>
-                    </div>
-                    <div className="control-box">
-                        <p> <i className='fas fa-chevron-right'></i> </p>
-                    </div>
-                    <div className="control-box">
-                        <p> <i className='fas fa-chevron-right'></i><i className='fas fa-chevron-right'></i> </p>
-                    </div>
-                    </div>
-                    <button className="afterTable">
-                    <a href="/patient-appointment.html">
-                    View all
-                    </a>
-                    </button>
-                </div>
+  return (
+    <div className="main">
+      <TopNav name="Appointments" />
+      <div className="main-content" style={{ display: "none" }}>
+        <div className="tableComponent">
+          <div className="tableTopRow">
+            <div>
+              <select name="label" className="filterOption">
+                <option value="all">All</option>
+                <option value="pending">Pending</option>
+                <option value="cancelled">Cancelled</option>
+                <option value="completed">Completed</option>
+              </select>
             </div>
+            <div>
+              <img src="./img/Elipses (3).png" className="elipses" alt="" />
+            </div>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Doctor Assigned</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Contact</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody id="table-body-ad"></tbody>
+          </table>
+          <div className="tableControls">
+            <div className="control-box">
+              <p>
+                {" "}
+                <i className="fas fa-chevron-left"></i>
+                <i className="fas fa-chevron-left"></i>{" "}
+              </p>
+            </div>
+            <div className="control-box">
+              <p>
+                {" "}
+                <i className="fas fa-chevron-left"></i>{" "}
+              </p>
+            </div>
+            <div className="control-box">
+              <p>1</p>
+            </div>
+            <div className="control-box">
+              <p>
+                {" "}
+                <i className="fas fa-chevron-right"></i>{" "}
+              </p>
+            </div>
+            <div className="control-box">
+              <p>
+                {" "}
+                <i className="fas fa-chevron-right"></i>
+                <i className="fas fa-chevron-right"></i>{" "}
+              </p>
+            </div>
+          </div>
+          <button className="afterTable">
+            <a href="/patient-appointment.html">View all</a>
+          </button>
         </div>
-    )
+      </div>
+
+      <div className={Appointment.background}>
+        <div className={Appointment.mainBackground}>
+          <h1>Book an Appointment</h1>
+          <div className={Appointment.formInput}>
+            <label className={Appointment.label} for="department">Department</label>
+            <select name="department" id="department">
+              <option value="">Choose a department</option>
+              <option value="gynaecology">Gynaecology</option>
+              <option value="pediatrics">Pediatrics</option>
+            </select>
+          </div>
+          <div className={Appointment.formInput}>
+            <label className={Appointment.label} for="appointment-date">Date</label>
+            <input type="date" id="appointment-date" />
+          </div>
+          <div className={Appointment.formInput}>
+            <label className={Appointment.label} for="appointment-time">Time</label>
+            <input type="time" id="appointment-time" />
+          </div>
+          <div className={Appointment.formInput}>
+            <label className={Appointment.label} for="appointment-details">
+              Appointment Details (Optional)
+            </label>
+            <textarea name="appointment-details" id="" cols="30" rows="4"></textarea>
+          </div>
+          <input
+            type="button"
+            value="BOOK APPOINTMENT"
+            class={Appointment.submitForm}
+            onclick="bookAnAppointment();"
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Appointments
+export default Appointments;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,34 +1,33 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Home from '../components/dashboard/Home'
-import SideNav from '../components/dashboard/layouts/SideNav'
-import Appointments from '../components/dashboard/Appointments'
-import HealthTips from '../components/dashboard/HealthTips'
-import Profile from '../components/dashboard/Profile'
-import Notifications from '../components/dashboard/Notifications'
-import Settings from '../components/dashboard/Settings'
-import '../components/styles/dashboard-template.css'
-import '../components/styles/appointments.css'
-import '../components/styles/patient-dashboard.css'
-import '../components/styles/settings.css'
-import '../components/styles/notifications.css'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../components/dashboard/Home";
+import SideNav from "../components/dashboard/layouts/SideNav";
+import Appointments from "../components/dashboard/Appointments";
+import HealthTips from "../components/dashboard/HealthTips";
+import Profile from "../components/dashboard/Profile";
+import Notifications from "../components/dashboard/Notifications";
+import Settings from "../components/dashboard/Settings";
+import "../components/styles/dashboard-template.css";
+import "../components/styles/appointments.css";
+import "../components/styles/patient-dashboard.css";
+import "../components/styles/settings.css";
+import "../components/styles/notifications.css";
 
 function Dashboard() {
-
-    return (
-        <Router>
-            <div className='container'>
-                <SideNav />
-                <Switch>
-                    <Route exact path='/dashboard' component={Home}></Route>
-                    <Route exact path='/appointments' component={Appointments}></Route>
-                    <Route exact path='/health-tips' component={HealthTips}></Route>
-                    <Route exact path='/profile' component={Profile}></Route>
-                    <Route exact path='/notifications' component={Notifications}></Route>
-                    <Route exact path='/settings' component={Settings}></Route>
-                </Switch>
-            </div>
-        </Router>
-    )
+  return (
+    <Router>
+      <div className="container">
+        <SideNav />
+        <Switch>
+          <Route exact path="/dashboard" component={Home}></Route>
+          <Route exact path="/appointments" component={Appointments}></Route>
+          <Route exact path="/health-tips" component={HealthTips}></Route>
+          <Route exact path="/profile" component={Profile}></Route>
+          <Route exact path="/notifications" component={Notifications}></Route>
+          <Route exact path="/settings" component={Settings}></Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
