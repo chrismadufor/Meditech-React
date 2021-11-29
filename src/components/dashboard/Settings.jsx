cf53486c798945280ea4689efa71dc1f56a804ab
Tolanee <68304958+Tolanee@users.noreply.github.com>
2021-11-29 22:37:25 +0100

    profile update

--- src/components/dashboard/Settings.jsx
+++ src/components/dashboard/Settings.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import TopNav from './layouts/TopNav'
-import Settingcss from '../styles/settings.module.css';
-
 
 function Settings() {
     return (
-      <div className={Settingcss.main}>
-        <TopNav name={Settingcss.Settings} />
-        <div className={Settingcss.mainContent}>
+      <div className='main'>
+        <TopNav name= 'Settings'/>
+        <div className='main-content'>
           <table>
          
          <tbody>
-           <tr className={Settingcss.tableSettings} >
+           <tr className="table-settings">
              <td><i className="fas fa-bell "></i></td>
-             <td><p> <a href="/"> Enable Push Notification</a></p></td>
+             <td><p> <a href="#"> Enable Push Notification</a></p></td>
              <td>
                <label className="switch">
                <input type="checkbox" />
@@ -23,15 +21,15 @@ function Settings() {
            </tr>
            <tr className="table-settings">
              <td> <i className="fas fa-cog "></i></td>
-             <td><p><a href="/"> Themes</a></p></td>
+             <td><p><a href="#"> Themes</a></p></td>
            </tr>
            <tr className="table-settings">
              <td><i className="fas fa-user"></i></td>
-             <td><p><a href="/"> Update Profile</a></p></td>
+             <td><p><a href="#"> Update Profile</a></p></td>
            </tr>
            <tr className="table-settings">
              <td><i className="fas fa-key "></i></td>
-             <td><p><a href="/">Change Password</a></p></td>
+             <td><p><a href="#">Change Password</a></p></td>
            </tr>
            
           
